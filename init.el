c415edc4b75e048721690989e7ef275783ed3c8d
Toc320A <liusy12810@outlook.com>
2021-03-31 14:29:15 +0800

    3.31 更新

--- init.el
+++ init.el
@@ -1,26 +1,61 @@
-;; the basic settings of emacs (I will not use now)
-(set-background-color "#010101")
-(set-foreground-color "#F5FFFA")
 
-(electric-pair-mode 1)
-(setq electric-pair-pairs '(
-			    (?\" . ?\")
-			    (?\( . ?\))
-			    (?\[ . ?\])
-			    (?\{ . ?\})
-			    ))
+;; video 8
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 
-;; "
+;; video 12
+(setq package-archives '(("org" . "http://elpa.emacs-china/org/")
+			 ("gnu" . "http://elpa.emacs-china.org/gnu/")
+			 ("melpa" . "http://elpa.emacs-china.org/melpa/")
+			 ("melpa-stable" . "http://elpa.emacs-china/melp-stable/")))
 
-(global-set-key (kbd "C-c C-x a w") 'loop-alpha)
-(setq alpha-list '((100 100) (95 65) (85 55) (75 45) (65 35)))
-(defun loop-alpha ()
-  (interactive)
-  (let ((h (car alpha-list)))                ;; head value will set to
-    ((lambda (a ab)
-       (set-frame-parameter (selected-frame) 'alpha (list a ab))
-       (add-to-list 'default-frame-alist (cons 'alpha (list a ab)))
-       ) (car h) (car (cdr h)))
-    (setq alpha-list (cdr (append alpha-list (list h))))
-    )
-)
+;; video 13 -- initialize and install package: use-package
+(setq package-check-signature nil)
+
+(require 'package)
+
+(unless (bound-and-true-p package--initialized)
+  (package-initialize))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; video 14 -- use-package initialize
+(setq use-package-always-ensure t
+      use-package-always-defer nil
+      use-package-always-demand t
+      use-package-expand-minimally t
+      use-package-verbose t)
+
+(require 'use-package)
+(use-package restart-emacs)
+
+;; video 15
+(use-package gruvbox-theme
+  :init (load-theme 'gruvbox-dark-soft))
+
+(use-package smart-mode-line
+  :init
+  (setq sml/no-confirm-load-theme t
+	sml/theme 'respectful)
+  (sml/setup))
+
+;; auto-variables setting
+;;!! do not touch this !!
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(restart-emacs use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
