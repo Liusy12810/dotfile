985cbdaa4291223678b8212eafd01d016739e4bf
Toc320A <liusy12810@outlook.com>
2021-04-17 21:52:10 +0800

    modified 4.17-21:52

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -38,4 +38,26 @@
   :config (setq swiper-action-recenter t
                 swiper-include-line-number-in-search t))
 
+(use-package company
+  :config
+  (setq company-dabbrev-code-everywhere t
+        company-dabbrev-code-modes t
+        company-dabbrev-code-other-buffers 'all
+        company-dabbrev-downcase nil
+        company-dabbrev-ignore-case t
+        company-dabbrev-other-buffers 'all
+        company-require-match nil
+        company-minimum-prefix-length 2
+        company-show-numbers t
+        company-tooltip-limit 20
+        company-idle-delay 0
+        company-echo-delay 0
+        company-tooltip-offset-display 'scrollbar
+        company-begin-commands '(self-insert-command))
+  (push '(company-semantic :with company-yasnippet) company-backends)
+  :hook ((after-init . global-company-mode)))
+
+(use-package flycheck
+  :hook (after-init . global-flycheck-mode))
+
 (provide 'init-packages)
